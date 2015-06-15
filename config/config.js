1834f378b3862f26a7242b1131ed0044adadd7f1
stove <stove654@gmail.com>
2015-06-16 02:55:53 +0700

    update database

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'ilovejavascript',
-    database: 'mongodb://localhost:27017/hottab',
+    database: 'mongodb://heroku_9m1xl023:79q71kdgglsi054samhmkesg39@ds047812.mongolab.com:47812/heroku_9m1xl023',
     userRoles: ['guest', 'user', 'admin']
 };
