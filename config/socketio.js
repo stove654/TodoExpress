287aaabb051d2032d0de8aef0d9fb8233991855b
stove <stove654@gmail.com>
2015-06-18 19:36:01 +0700

    push socket

--- config/socketio.js
+++ config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
 
   // Insert sockets below
   require('../api/thing/thing.socket').register(socket);
+  require('../api/order/order.socket').register(socket);
 }
 
 module.exports = function (socketio) {
