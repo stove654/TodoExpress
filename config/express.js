72f09f0b8beb0978c85ea2e099be07912122e235
loihoang <stove2192@gmail.com>
2016-08-30 11:37:35 +0700

    default app

--- config/express.js
+++ config/express.js
@@ -15,7 +15,6 @@ var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 var cors = require('cors');
-var multer  = require('multer');
 module.exports = function(app) {
 
   app.use(cors());
@@ -25,19 +24,6 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-
-  var done=false;
-  app.use(multer({ dest: './uploads/',
-    rename: function (fieldname, filename) {
-      console.log(filename);
-      return filename;
-    },
-    onFileUploadStart: function (file) {
-    },
-    onFileUploadComplete: function (file) {
-      done=true;
-    }
-  }));
 
   app.use(function(req, res, next) {
     res.contentType('application/json');
